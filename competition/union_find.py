ad8ed8851c95dd3f6b094b22aea8200dc15107a8
Lucien Zhang <lucien_zhang@hotmail.com>
2019-06-01 21:56:42 +0900

    better union find

--- competition/union_find.py
+++ competition/union_find.py
@@ -2,7 +2,7 @@
 # @Author: Lucien Zhang
 # @Date:   2019-03-31 16:39:53
 # @Last Modified by:   Lucien Zhang
-# @Last Modified time: 2019-03-31 16:45:44
+# @Last Modified time: 2019-06-01 21:54:27
 
 
 class UnionFind(object):
@@ -39,6 +39,46 @@ class UnionFind(object):
         return sets
 
 
+class UnionFind2(object):
+    def __init__(self):
+        self.forest = {}
+        self.size = 0
+
+    def find(self, element):
+        if element not in self.forest:
+            return None
+        root = element
+        while self.forest[root] != root:
+            root = self.forest[root]
+        while self.forest[element] != root:
+            self.forest[element], element = root, self.forest[element]
+        return root
+
+    def union(self, x, y):
+        rx = self.find(x)
+        ry = self.find(y)
+        if rx == ry:
+            return
+        self.forest[ry] = rx
+        self.size -= 1
+
+    def add(self, x):
+        self.forest[x] = x
+        self.size += 1
+
+    def get_sets(self):
+        sets = {}
+        for k in self.forest.keys():
+            root = self.find(k)
+            if root == k and root not in sets:
+                sets[root] = [root]
+            elif root in sets:
+                sets[root].append(k)
+            else:
+                sets[root] = [root, k]
+        return list(sets.values())
+
+
 if __name__ == "__main__":
     """
     0-1  4-5
@@ -53,7 +93,9 @@ if __name__ == "__main__":
          [0, 0, 0, 0, 1, 1, 0],
          [0, 0, 0, 0, 1, 0, 1]]
 
-    ufs = [UnionFind(len(G))]
+    ufs = [UnionFind(len(G)), UnionFind2()]
+    for i in range(len(G)):
+        ufs[1].add(i)
     for uf in ufs:
         # consider upper triangle
         for i in range(len(G) - 1):
@@ -65,3 +107,4 @@ if __name__ == "__main__":
         for i in range(len(sets)):
             print("{}-th set is".format(i))
             print(sets[i])
+        print('-' * 20)
