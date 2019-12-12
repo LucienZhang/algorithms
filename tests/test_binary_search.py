5dab7f3978462b79f11c403612d982caf4d2dbb5
Lucien <lucienzhangzl@outlook.com>
2019-12-12 18:39:38 +0900

    test ini

--- tests/test_binary_search.py
+++ tests/test_binary_search.py
@@ -1,22 +1,28 @@
 import sys
 from pathlib import Path
+import pytest
 
 base_dir = Path(__file__).resolve().parent.parent
 sys.path.append(str(base_dir))
-from algorithms.binary_search import binary_search
 
 
+def base_binary_search(func):
+    assert func([], 1) == -1
+    assert func([1], -1) == -1
+    assert func([1], 1) == 0
+    assert func([1], 2) == -1
+    assert func([1, 2], -1) == -1
+    assert func([1, 2], 1) == 0
+    assert func([1, 2], 2) == 1
+    assert func([1, 2], 3) == -1
+    assert func([1, 2, 2, 2, 3], -1) == -1
+    assert func([1, 2, 2, 2, 3], 1) == 0
+    assert func([1, 2, 2, 2, 3], 2) in [1, 2, 3]
+    assert func([1, 2, 2, 2, 3], 3) == 4
+    assert func([1, 2, 2, 2, 3], 4) == -1
+
+
+@pytest.mark.sample
 def test_binary_search():
-    assert binary_search([], 1) == -1
-    assert binary_search([1], -1) == -1
-    assert binary_search([1], 1) == 0
-    assert binary_search([1], 2) == -1
-    assert binary_search([1, 2], -1) == -1
-    assert binary_search([1, 2], 1) == 0
-    assert binary_search([1, 2], 2) == 1
-    assert binary_search([1, 2], 3) == -1
-    assert binary_search([1, 2, 2, 2, 3], -1) == -1
-    assert binary_search([1, 2, 2, 2, 3], 1) == 0
-    assert binary_search([1, 2, 2, 2, 3], 2) in [1, 2, 3]
-    assert binary_search([1, 2, 2, 2, 3], 3) == 4
-    assert binary_search([1, 2, 2, 2, 3], 4) == -1
+    from algorithms.binary_search import binary_search as bs_sample
+    base_binary_search(bs_sample)
